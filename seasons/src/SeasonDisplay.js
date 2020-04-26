657972e04bd44bb9e51b31c670ca09eff424e8b9
Yuxuan Zhu <zyx97agm@gmail.com>
2020-04-26 20:43:32 +0800

    style the ui

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,4 +1,18 @@
+// import css file into project
+// 当我们加载css文件时, webpack工具会从文件中获取内容并将其交给index.html进行使用
+import "./SeasonDisplay.css";
 import React from "react";
+
+const seasonConfig = {
+	summer: {
+		text: "夏天来啦！",
+		iconName: "massive sun icon",
+	},
+	winter: {
+		text: "我爱冬天！",
+		iconName: "massive snowflake icon",
+	},
+};
 
 // 决定现在是什么season
 const getSeason = (lat, month) => {
@@ -11,8 +25,14 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = (props) => {
 	const season = getSeason(props.lat, new Date().getMonth());
-	console.log(season);
-	return <div>Season display</div>;
+	const { text, iconName } = seasonConfig[season]; // {text: icon: }
+	return (
+		<div className={`season-disply ${season}`}>
+			<i className={`icon-left ${iconName}`}></i>
+			<h1>{text}</h1>
+			<i className={`icon-right ${iconName}`}></i>
+		</div>
+	);
 };
 
 export default SeasonDisplay;
--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 class App extends React.Component {
 	state = { lat: null, errorMes: "" };
@@ -15,7 +16,7 @@ class App extends React.Component {
 			return <div>Error: {this.state.errorMes}</div>;
 		}
 
-		return <div>Loading</div>;
+		return <Spinner />;
 	}
 
 	componentDidMount() {
