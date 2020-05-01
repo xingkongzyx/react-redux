e7b18e3c7c5ecc98e610f4312d4100754220f979
Yuxuan Zhu <zyx97agm@gmail.com>
2020-05-01 22:15:30 +0800

    finish pics app

--- pics/src/components/ImageCard.js
+++ pics/src/components/ImageCard.js
@@ -6,20 +6,23 @@ class ImageCard extends React.Component {
 	constructor(props) {
 		super(props);
 		this.imageRef = React.createRef();
+		this.state = { spans: 0 };
 	}
 
 	componentDidMount() {
 		this.imageRef.current.addEventListener("load", this.setSpans);
-    }
-    
+	}
+
 	setSpans = () => {
-		console.log(this.imageRef.current.clientHeight);
-    };
-    
+		const height = this.imageRef.current.clientHeight;
+		const spans = Math.ceil(height / 10);
+		this.setState({ spans });
+	};
+
 	render() {
 		const { description, urls } = this.props.image;
 		return (
-			<div>
+			<div style={{ gridRowEnd: `span ${this.state.spans}` }}>
 				<img ref={this.imageRef} alt={description} src={urls.regular} />
 			</div>
 		);
