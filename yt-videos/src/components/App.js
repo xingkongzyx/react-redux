a704e1dde857354a4245b0201879c406211138ba
Yuxuan Zhu <zyx97agm@gmail.com>
2020-05-02 23:54:12 +0800

    basic searchbar implementation

---
+++ yt-videos/src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import SearchBar from "../components/SearchBar";
+
+class App extends React.Component {
+	render() {
+		return (
+			<div className="ui container">
+				<SearchBar />
+			</div>
+		);
+	}
+}
+
+export default App;
