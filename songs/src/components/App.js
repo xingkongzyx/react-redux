c71884c53da9b7dad9a7b20439ab8c0d9af9f694
Yuxuan Zhu <zyx97agm@gmail.com>
2020-06-05 10:39:43 +0800

    create song detail component

--- songs/src/components/App.js
+++ songs/src/components/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import SongList from "./SongList";
+import SongDetail from "./SongDetail";
 // 如果不明确声明文件而只是声明目录，webpack将默认使用index.js文件
 // 使用{}意味着我们想要import named export from the file
 import { selectSong } from "../actions";
@@ -10,6 +11,7 @@ const App = () => {
 			<div className="ui row">
 				<div className="column eight wide">
 					<SongList />
+					<SongDetail />
 				</div>
 			</div>
 		</div>
--- songs/src/components/SongList.js
+++ songs/src/components/SongList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
+import { selectSong } from "../actions";
 
 class SongList extends React.Component {
 	// return JSX code
@@ -8,7 +9,12 @@ class SongList extends React.Component {
 			return (
 				<div className="item" key={song.title}>
 					<div className="right floated content">
-						<button className="ui button primary">
+						<button
+							className="ui button primary"
+							onClick={() => {
+								this.props.selectSong(song);
+							}}
+						>
 							Select
 						</button>
 					</div>
@@ -19,14 +25,22 @@ class SongList extends React.Component {
 	}
 
 	render() {
-		
-		return <div className="ui divided list">{this.renderList()} </div>;
+		return (
+			<div className="ui divided list">
+				{this.renderList()}{" "}
+			</div>
+		);
 	}
 }
 
 /* 将store中的state(all data)传入function并进行一些计算, 
 最后使数据以props的形式显示于component中 */
 const mapStatetToProps = (state) => {
+	console.log(state);
 	return { songs: state.songs };
 };
-export default connect(mapStatetToProps)(SongList);
+
+// connect func会将selectSong action creator 作为props传入component
+export default connect(mapStatetToProps, { selectSong })(SongList);
+
+
