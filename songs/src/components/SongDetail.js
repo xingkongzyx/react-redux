d93a3795040f60ae328cd5ec5b197de2c5b9118d
Yuxuan Zhu <zyx97agm@gmail.com>
2020-06-05 15:00:42 +0800

    finish app

--- songs/src/components/SongDetail.js
+++ songs/src/components/SongDetail.js
@@ -1,12 +1,24 @@
 import React from "react";
 import { connect } from "react-redux";
 
-const SongDetail = (props) => {
-	console.log(props);
-	return <div>Song Detail</div>;
+const SongDetail = ({ song }) => {
+	if (song) {
+		return (
+			<div>
+				<h3>Details for:</h3>
+				<p>
+					Title: {song.title} <br></br>Duration:{" "}
+					{song.duration}
+				</p>
+			</div>
+		);
+	} else {
+		return <div>Select a Song</div>;
+	}
 };
 
 const mapStateToProp = (state) => {
+	// 这里返回的object会以props的形式在component中展现
 	return { song: state.selectedSong };
 };
 
