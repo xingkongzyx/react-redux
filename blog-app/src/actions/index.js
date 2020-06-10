c72afbef9c326bce0eaea46400ad9d406f7401b1
Yuxuan Zhu <zyx97agm@gmail.com>
2020-06-10 12:37:55 +0800

    small fix for console warnings

--- blog-app/src/actions/index.js
+++ blog-app/src/actions/index.js
@@ -1,5 +1,6 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
-
+// import _ from "lodash";
+// fetch all blogs at a time
 export const fetchPosts = () => {
 	return async (dispatch) => {
 		const response = await jsonPlaceholder.get("/posts");
@@ -9,3 +10,24 @@ export const fetchPosts = () => {
 		});
 	};
 };
+
+// fetch one user at a time
+export const fetchUser = function (id) {
+	return async function (dispatch) {
+		const response = await jsonPlaceholder.get("./users/" + id);
+		dispatch({
+			type: "FETCH_USER",
+			payload: response.data,
+		});
+	};
+};
+// // fetch one user at a time
+// export const fetchUser = (id) => {
+// 	return async (dispatch) => {
+// 		const response = await jsonPlaceholder.get("./users/" + id);
+// 		dispatch({
+// 			type: "FETCH_USER",
+// 			payload: response.data,
+// 		});
+// 	};
+// };
--- blog-app/src/components/PostList.js
+++ blog-app/src/components/PostList.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { connect } from "react-redux";
 import { fetchPosts } from "../actions";
+import UserHeader from "./UserHeader";
 class PostList extends React.Component {
+	// 在此method中调用action creator
 	componentDidMount() {
-		// console.log(this.props.posts.data);
 		this.props.fetchPosts();
 	}
 
@@ -17,6 +18,7 @@ class PostList extends React.Component {
 							<h2>{post.title}</h2>
 							<p>{post.body}</p>
 						</div>
+						<UserHeader userId={post.userId} />
 					</div>
 				</div>
 			);
@@ -24,7 +26,6 @@ class PostList extends React.Component {
 	}
 
 	render() {
-		console.log(this.props.posts);
 		return (
 			<div className="ui relaxed divided list">
 				{this.renderList()}
