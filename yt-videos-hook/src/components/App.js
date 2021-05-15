e3304daa701ef97770191a277256c0464e3ccd62
Yuxuan Zhu <zyx97agm@gmail.com>
2021-05-15 23:33:26 +0800

    create custom hook

--- yt-videos-hook/src/components/App.js
+++ yt-videos-hook/src/components/App.js
@@ -1,60 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SearchBar from "../components/SearchBar";
 import VideoList from "./VideoList";
 import VideoDetail from "./VideoDetail";
 import youtube from "../apis/youtube";
 
-const KEY = "AIzaSyBf-mBYkmKoyfGEBInvrtMxeZuMQBQGiho";
-class App extends React.Component {
-	// 用于将搜到的videos储存于state中
-	state = { videos: [], selectedVideo: null };
+const App = () => {
+	const [selectedVideo, setSelectedVideo] = useState(null);
 
-	// 用于设置页面第一次启动时的默认视频搜索关键词(设置为Google)
-	componentDidMount = () => {
-		this.onTermSubmit("Google");
-	};
+	// 同时设置selectedVideo为返回结果中第一个视频是为了确保只要用户一提交搜素表格
+	// 就能有iframe显示,使得页面美观
+	setSelectedVideo(items[0]);
 
-	onVideoSelect = (video) => {
-		this.setState({ selectedVideo: video });
-	};
-
-	onTermSubmit = async (term) => {
-		const response = await youtube.get("/search", {
-			params: {
-				q: term,
-				part: "snippet",
-				maxResults: 5,
-				type: "video",
-				key: KEY,
-			},
-		});
-		const { items } = response.data;
-		// 同时设置selectedVideo为返回结果中第一个视频是为了确保只要用户一提交搜素表格
-		// 就能有iframe显示,使得页面美观
-		this.setState({ videos: items, selectedVideo: items[0] });
-	};
-	render() {
-		return (
-			<div className="ui container">
-				<SearchBar onFormSubmit={this.onTermSubmit} />
-				<div className="ui grid">
-					<div className="ui row">
-						<div className="eleven wide column">
-							<VideoDetail
-								video={this.state.selectedVideo}
-							/>
-						</div>
-						<div className="five wide column">
-							<VideoList
-								onVideoSelect={this.onVideoSelect}
-								videos={this.state.videos}
-							/>
-						</div>
+	return (
+		<div className="ui container">
+			<SearchBar onFormSubmit={onTermSubmit} />
+			<div className="ui grid">
+				<div className="ui row">
+					<div className="eleven wide column">
+						<VideoDetail video={selectedVideo} />
+					</div>
+					<div className="five wide column">
+						<VideoList
+							onVideoSelect={setSelectedVideo}
+							videos={videos}
+						/>
 					</div>
 				</div>
 			</div>
-		);
-	}
-}
+		</div>
+	);
+};
 
 export default App;
---
+++ yt-videos-hook/src/hooks/useVideos.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+const KEY = "AIzaSyBf-mBYkmKoyfGEBInvrtMxeZuMQBQGiho";
+
+const useVideos = (defaultSearchTerm) => {
+	// 用于将搜到的videos储存于state中
+	const [videos, setVideos] = useState([]);
+
+	// 用于设置页面第一次启动时的默认视频搜索关键词(设置为React)
+	useEffect(() => {
+		onTermSubmit(defaultSearchTerm);
+	}, []);
+
+	const search = async (term) => {
+		const response = await youtube.get("/search", {
+			params: {
+				q: term,
+				part: "snippet",
+				maxResults: 5,
+				type: "video",
+				key: KEY,
+			},
+		});
+		const { items } = response.data;
+
+		setVideos(items);
+	};
+
+	// return the ouput 这里指videos和搜素videos的function
+	return [videos, search];
+	// 也可以使用返回object的形式
+	// return { videos, search };
+};
+
+export default useVideos;
