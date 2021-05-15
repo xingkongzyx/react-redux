4821e4d4e5114caf1727117c0bc067bd50ab0970
Yuxuan Zhu <zyx97agm@gmail.com>
2021-05-15 19:37:00 +0800

    refactor searchbar file

--- yt-videos-hook/src/components/SearchBar.js
+++ yt-videos-hook/src/components/SearchBar.js
@@ -1,35 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
-class SearchBar extends React.Component {
-	state = { term: "" };
-	// 当用户更改input中信息时调用
-	onInputChange = (e) => {
-		this.setState({ term: e.target.value });
-	};
-	// 当用户提交表单时调用
-	onFormSubmit = (e) => {
-		e.preventDefault();
-		// 使用从App component中传入的callback
-		this.props.onFormSubmit(this.state.term);
-	};
-	render() {
-		return (
-			<div className="search-bar ui segment" onSubmit={this.onFormSubmit}>
-				<form className="ui form">
-					<div className="field">
-						<label>Video Search</label>
-						<input
-							type="text"
-							value={this.state.term}
-							placeholder="Please enter the keyword you want to search!"
-							// "onChange" is a special name for event handler
-							onChange={this.onInputChange}
-						/>
-					</div>
-				</form>
-			</div>
-		);
-	}
-}
+const SearchBar = ({ onFormSubmit }) => {
+    const [term, setTerm] = useState("");
+    // 当用户更改input中信息时调用
+    const onInputChange = (e) => {
+        setTerm(e.target.value);
+    };
+    // 当用户提交表单时调用
+    const onSubmit = (e) => {
+        e.preventDefault();
+        // 使用从App component中传入的callback
+        onFormSubmit(term);
+    };
+
+    return (
+        <div className="search-bar ui segment" onSubmit={onSubmit}>
+            <form className="ui form">
+                <div className="field">
+                    <label>Video Search</label>
+                    <input
+                        type="text"
+                        value={term}
+                        placeholder="Please enter the keyword you want to search!"
+                        // "onChange" is a special name for event handler
+                        onChange={onInputChange}
+                    />
+                </div>
+            </form>
+        </div>
+    );
+};
 
 export default SearchBar;
